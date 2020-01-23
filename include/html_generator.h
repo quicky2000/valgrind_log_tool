f4e1f5de7ae8e210f1e99bdd5339fdf3e917ffc4
Julien Thevenon <julien_thevenon@yahoo.fake.fr>
2020-01-23 15:42:21 +0100

    Add object links

--- include/html_generator.h
+++ include/html_generator.h
@@ -64,6 +64,17 @@ namespace valgrind_log_tool
         inline
         std::string get_file_link(const std::string & p_kind) const;
 
+        /**
+         * Compute object id that will be used as local anchor
+         * @param p_object string representing error frame object
+         * @return string representing object id
+         */
+        inline
+        std::string get_object_id(const std::string & p_object) const;
+
+        inline
+        std::string get_object_link(const std::string & p_object) const;
+
         inline
         std::string get_error_id(const valgrind_error & p_error) const;
 
@@ -77,6 +88,9 @@ namespace valgrind_log_tool
         void collect_file_info(const valgrind_log_content & p_content);
 
         inline
+        void collect_object_info(const valgrind_log_content & p_content);
+
+        inline
         void generate_html(const valgrind_error & p_error);
 
         inline
@@ -84,6 +98,9 @@ namespace valgrind_log_tool
 
         inline
         void generate_files_html(const valgrind_log_content & p_content);
+
+        inline
+        void generate_objects_html(const valgrind_log_content & p_content);
 
         std::ofstream m_file;
 
@@ -106,6 +123,16 @@ namespace valgrind_log_tool
          * files sorted per number of occurence
          */
         std::multimap<unsigned int, std::string> m_sorted_files;
+
+        /**
+         * List of frame objects and associated id
+         */
+        std::map<std::string, unsigned int> m_objects;
+
+        /**
+         * Objects sorted per number of occurence
+         */
+        std::multimap<unsigned int, std::string> m_sorted_objects;
 
     };
 
@@ -159,8 +186,19 @@ namespace valgrind_log_tool
         }
         m_file << "</ul>" << std::endl;
 
+        collect_object_info(p_content);
+
+        m_file << "<H2>Encountered Objects</H2>" << std::endl;
+        m_file << "<ul>" << std::endl;
+        for(const auto & l_iter: m_sorted_objects)
+        {
+            m_file << "<li>" << get_object_link(l_iter.second) << " : " << l_iter.first << "</li>" << std::endl;
+        }
+        m_file << "</ul>" << std::endl;
+
         generate_files_html(p_content);
         generate_kinds_html(p_content);
+        generate_objects_html(p_content);
 
         const auto l_treat_error = [&](const valgrind_error & p_error)
         {
@@ -214,7 +252,7 @@ namespace valgrind_log_tool
         {
             m_file << "<tr>" << std::endl;
             //m_file << "<td>" << p_frame.get_ip() << "</td>" << std::endl;
-            m_file << "<td>" << p_frame.get_obj() << "</td>" << std::endl;
+            m_file << "<td>" << (!p_frame.get_obj().empty() ? get_object_link(p_frame.get_obj()) : "") << "</td>" << std::endl;
             m_file << "<td>" << p_frame.get_fn() << "</td>" << std::endl;
             m_file << "<td>" << p_frame.get_dir() << "</td>" << std::endl;
             m_file << "<td>" << (!p_frame.get_file().empty() ? get_file_link(p_frame.get_file()) : "" )<< "</td>" << std::endl;
@@ -265,6 +303,22 @@ namespace valgrind_log_tool
     html_generator::get_file_link(const std::string & p_kind) const
     {
         return "<a href=\"#" + get_file_id(p_kind) + "\">" + p_kind + "</a>";
+    }
+
+    //-------------------------------------------------------------------------
+    std::string
+    html_generator::get_object_id(const std::string & p_object) const
+    {
+        auto l_iter = m_objects.find(p_object);
+        assert(m_objects.end() != l_iter);
+        return "Object_" + std::to_string(l_iter->second);
+    }
+
+    //-------------------------------------------------------------------------
+    std::string
+    html_generator::get_object_link(const std::string & p_object) const
+    {
+        return "<a href=\"#" + get_object_id(p_object) + "\">" + p_object + "</a>";
     }
 
     //-------------------------------------------------------------------------
@@ -428,6 +482,103 @@ namespace valgrind_log_tool
         }
 
     }
+
+    //-------------------------------------------------------------------------
+    void
+    html_generator::collect_object_info(const valgrind_log_content & p_content)
+    {
+        // List all objects
+        m_objects.clear();
+        std::map<std::string, unsigned int> l_object_number;
+        l_object_number.clear();
+        const auto l_collected_objects_from_frame = [&](const valgrind_frame & p_frame)
+        {
+            const std::string & l_object_name = p_frame.get_obj();
+            if(!l_object_name.empty() && !m_objects.count(l_object_name))
+            {
+                m_objects.insert(std::pair<std::string, unsigned int>(l_object_name, m_objects.size()));
+                l_object_number.insert(make_pair(l_object_name, 0));
+            }
+        };
+
+        const auto l_collect_objects_from_error = [&](const valgrind_error & p_error)
+        {
+            p_error.process_stack(l_collected_objects_from_frame);
+        };
+        p_content.process_errors(l_collect_objects_from_error);
+
+        // Count objects (once per error)
+        for(auto l_iter: m_objects)
+        {
+            const auto l_count_object_in_errors = [&](const valgrind_error & p_error)
+            {
+                bool l_appear_in_error_stack = false;
+                const auto l_search_objects_in_frame = [&](const valgrind_frame & p_frame)
+                {
+                    if (l_iter.first == p_frame.get_obj())
+                    {
+                        l_appear_in_error_stack = true;
+                    }
+                };
+                p_error.process_stack(l_search_objects_in_frame);
+                if(l_appear_in_error_stack)
+                {
+                    l_object_number[l_iter.first]++;
+                }
+            };
+            p_content.process_errors(l_count_object_in_errors);
+        }
+        m_sorted_objects.clear();
+        for(const auto & l_iter:l_object_number)
+        {
+            m_sorted_objects.insert(std::pair<unsigned int, std::string>(l_iter.second, l_iter.first));
+        }
+    }
+
+    //-------------------------------------------------------------------------
+    void
+    html_generator::generate_objects_html(const valgrind_log_content & p_content)
+    {
+        m_file << "<H2>Errors per objects</H2>" << std::endl;
+        for(const auto & l_iter: m_sorted_objects)
+        {
+            bool l_first = true;
+            const std::string & l_object_name = l_iter.second;
+            m_file << "<hr id=\"" << get_object_id(l_object_name) << "\">" << std::endl;
+            m_file << "Errors whose call stack mention object <b>" << l_object_name << "</b>" << std::endl;
+
+            bool l_contain = false;
+            const auto l_frame_contain_object = [&](const valgrind_frame & p_frame)
+            {
+                if(l_object_name == p_frame.get_obj())
+                {
+                    l_contain = true;
+                }
+            };
+            const auto l_collecter_errors_per_object = [&](const valgrind_error & p_error)
+            {
+                l_contain = false;
+                p_error.process_stack(l_frame_contain_object);
+                if(l_contain)
+                {
+                    if(!l_first)
+                    {
+                        m_file << ", ";
+                    }
+                    else
+                    {
+                        l_first = false;
+                    }
+                    m_file << get_error_link(p_error);
+                }
+            };
+            m_file << "<ul><li>" << std::endl;
+            p_content.process_errors(l_collecter_errors_per_object);
+            m_file << "</li></ul>" << std::endl;
+        }
+
+    }
+
 
 }
 #endif //VALGRIND_LOG_TOOL_HTML_GENERATOR_H
